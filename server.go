6ff87be2774c28a8da6f83543b5476bf25c74ff3
Pongpanot Chuaysakun <muchrm.me@gmail.com>
2017-09-27 02:08:02 +0700

    update return response

--- server.go
+++ server.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/", getUser)
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", getHome)
 	e.Logger.Fatal(e.Start(":3000"))
 }
-func getUser(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+
+type ResponseContent struct {
+	Response  string    `json:"response"`
+	Timestamp time.Time `json:"timestamp"`
+	Random    int       `json:"random"`
+}
+
+func getHome(c echo.Context) error {
+	content := ResponseContent{"Sent via JSONP", time.Now().UTC(), rand.Intn(1000)}
+	return c.JSON(http.StatusOK, content)
 }
