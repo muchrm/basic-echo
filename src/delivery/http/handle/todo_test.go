228f404e3fcdefe15f6fe43ae109d0757350b422
Pongpanot Chuaysakun <muchrm.me@gmail.com>
2017-10-04 23:51:45 +0700

    .

--- src/delivery/http/handle/todo_test.go
+++ src/delivery/http/handle/todo_test.go
@@ -1,1 +1,16 @@
 package handle
+
+import (
+	"testing"
+
+	"github.com/labstack/echo"
+	"github.com/muchrm/go-echo/src/infrastructure/repository/demodb"
+	todoUsecase "github.com/muchrm/go-echo/src/usecase"
+)
+
+func TestNewTodoHandle(t *testing.T) {
+	todoRepository := demodb.NewTodo()
+	todoInteractor := todoUsecase.NewTodoInterActor(todoRepository)
+	NewTodoHandle(todoInteractor, echo.New().Group("/api"))
+	t.Skip("skill")
+}
--- src/infrastructure/repository/demodb/todo_test.go
+++ src/infrastructure/repository/demodb/todo_test.go
@@ -1,1 +1,17 @@
 package demodb
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewTodo(t *testing.T) {
+	NewTodo()
+	t.Skip("skipping test")
+}
+func TestGetTodoList(t *testing.T) {
+	todo := NewTodo()
+	_, err := todo.GetTodoList()
+	assert.Nil(t, err)
+}
